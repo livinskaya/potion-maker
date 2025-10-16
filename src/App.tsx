56d47f6a66d67ffcceead358ee03915d5bf9a034
Lando Winzen <winzenlando@gmail.com>
2025-10-16 17:57:02 +0000

    first step (beta)

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
-// src/App.tsx
 
 export default function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-900">
-      {/* Cauldron */}
       <div className="relative w-56 h-40">
         <div className="absolute left-0 top-0 w-56 h-42 overflow-hidden">
           <div className="absolute bottom-1 w-56 h-48 bg-[#2D4158] rounded-full z-10"></div>
-
           <div className="absolute top-0 w-full h-6 bg-[#2D4158] rounded-xl z-10"></div>
           <div className="absolute top-2 left-1 w-[90%] h-8 bg-[#2D4158] rounded-full z-10"></div>
-
-          {/* Cauldron legs */}
           <div className="absolute -bottom-6 left-4 w-8 h-28 bg-[#2D4158] rounded-tl-[80%] rounded-tr-[50%] rotate-[160deg] skew-x-[-30deg] scale-y-[0.466]"></div>
           <div className="absolute -bottom-6 right-4 w-8 h-28 bg-[#2D4158] rounded-tl-[80%] rounded-tr-[50%] rotate-[120deg] skew-x-[-30deg] scale-y-[0.466]"></div>
           <div className="absolute -bottom-6 right-12 w-8 h-28 bg-[#2D4158] rounded-tl-[80%] rounded-tr-[50%] rotate-[120deg] skew-x-[-30deg] scale-y-[0.466]"></div>
-
-          {/* Goo blobs */}
           <div className="absolute top-0 left-20 w-16 h-16 bg-[#66CC66] rounded-full animate-bob"></div>
           <div className="absolute top-2 right-28 w-14 h-14 bg-[#E85256] rounded-full animate-bob delay-300"></div>
         </div>
