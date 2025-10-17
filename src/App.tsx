84d33866df5dd6e69df232aef52480848e32fb2e
Lando Winzen <winzenlando@gmail.com>
2025-10-17 20:20:29 +0200

    18.10

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 export default function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-900">
@@ -16,4 +15,6 @@ export default function App() {
       </div>
     </div>
   );
+
+  //big programm stuff on 18.10
 }
